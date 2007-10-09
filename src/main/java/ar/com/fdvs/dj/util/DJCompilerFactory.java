9b1816557ff74a1e05181ba5dd2180e56427ab0f
djmamana <djmamana@2bfdcaec-5528-0410-9d21-f7f2b6497bdd>
2007-10-09 14:44:29 +0000

    git-svn-id: https://dynamicjasper.svn.sourceforge.net/svnroot/dynamicjasper/api/trunk@210 2bfdcaec-5528-0410-9d21-f7f2b6497bdd

--- src/main/java/ar/com/fdvs/dj/util/DJCompilerFactory.java
+++ src/main/java/ar/com/fdvs/dj/util/DJCompilerFactory.java
@@ -1,5 +1,7 @@
 package ar.com.fdvs.dj.util;
 
+import net.sf.jasperreports.engine.util.JRClassLoader;
+
 /**
  * @author Alejandro Gomez (alejandro.gomez@fdvsolutions.com)
  *         Date: Oct 8, 2007
@@ -8,9 +10,16 @@ package ar.com.fdvs.dj.util;
 public class DJCompilerFactory {
 
     public static String getCompilerClassName() {
-        if (DJJRJdtCompiler.isValid()) {
-            return DJJRJdtCompiler.class.getName();
-        } else if (DJJRJdk13Compiler.isValid()) {
+        try {
+            if (JRClassLoader.loadClassForName("org.eclipse.jdt.internal.compiler.Compiler") != null)
+            	 return DJJRJdtCompiler.class.getName();
+        } catch (ClassNotFoundException ex) {
+        	//nothing to do
+        } catch (NoClassDefFoundError e) {
+        	//nothing to do
+		}    	
+
+    	if (DJJRJdk13Compiler.isValid()) {
             return DJJRJdk13Compiler.class.getName();
         } else if (DJJRJdk12Compiler.isValid()) {
             return DJJRJdk12Compiler.class.getName();
