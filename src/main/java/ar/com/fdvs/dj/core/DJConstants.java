8941abd419975b7e2c20ad77f5b713be512ac31e
djmamana <djmamana@2bfdcaec-5528-0410-9d21-f7f2b6497bdd>
2008-06-27 06:43:02 +0000

    git-svn-id: https://dynamicjasper.svn.sourceforge.net/svnroot/dynamicjasper/api/trunk@383 2bfdcaec-5528-0410-9d21-f7f2b6497bdd

--- src/main/java/ar/com/fdvs/dj/core/DJConstants.java
+++ src/main/java/ar/com/fdvs/dj/core/DJConstants.java
@@ -29,7 +29,10 @@
 
 package ar.com.fdvs.dj.core;
 
+import net.sf.jasperreports.engine.JRQuery;
 import net.sf.jasperreports.engine.JRReport;
+import net.sf.jasperreports.engine.design.JRDesignQuery;
+import net.sf.jasperreports.engine.util.JRProperties;
 
 public interface DJConstants {
 
