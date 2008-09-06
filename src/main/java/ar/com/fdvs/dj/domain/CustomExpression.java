9fcd55f25f042911772fdb7fbf027237fd07a3a5
djmamana <djmamana@2bfdcaec-5528-0410-9d21-f7f2b6497bdd>
2008-09-06 18:19:45 +0000

    git-svn-id: https://dynamicjasper.svn.sourceforge.net/svnroot/dynamicjasper/api/tags/release_2_1_0@540 2bfdcaec-5528-0410-9d21-f7f2b6497bdd

---
+++ src/main/java/ar/com/fdvs/dj/domain/CustomExpression.java
@@ -0,0 +1,48 @@
+/*
+ * DynamicJasper: A library for creating reports dynamically by specifying
+ * columns, groups, styles, etc. at runtime. It also saves a lot of development
+ * time in many cases! (http://sourceforge.net/projects/dynamicjasper)
+ *
+ * Copyright (C) 2008  FDV Solutions (http://www.fdvsolutions.com)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ *
+ * License as published by the Free Software Foundation; either
+ *
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ *
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ *
+ */
+
+package ar.com.fdvs.dj.domain;
+
+import ar.com.fdvs.dj.domain.entities.conditionalStyle.StatusLightCondition;
+
+/**
+ * Powerful interface. The idea behind this is that anybody could extend </br>
+ * this class and redefine the evaluate method to show different results in a cell. </br>
+ * Example: date formatting, money formatting, string trimming, etc. </br>
+ * </br>
+ * @see StatusLightCondition
+ * </br>
+ */
+public interface CustomExpression {
+
+	public static final String EVAL_METHOD_NAME = "evaluate";
+
+	public Object evaluate(Object object);
+
+}
---
+++ src/test/java/ar/com/fdvs/dj/test/FontReportTest.java
@@ -0,0 +1,81 @@
+/*
+ * DynamicJasper: A library for creating reports dynamically by specifying
+ * columns, groups, styles, etc. at runtime. It also saves a lot of development
+ * time in many cases! (http://sourceforge.net/projects/dynamicjasper)
+ *
+ * Copyright (C) 2008  FDV Solutions (http://www.fdvsolutions.com)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ *
+ * License as published by the Free Software Foundation; either
+ *
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ *
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ *
+ */
+
+package ar.com.fdvs.dj.test;
+
+
+import java.util.Date;
+
+import net.sf.jasperreports.view.JasperViewer;
+import ar.com.fdvs.dj.domain.DynamicReport;
+import ar.com.fdvs.dj.domain.Style;
+import ar.com.fdvs.dj.domain.builders.FastReportBuilder;
+import ar.com.fdvs.dj.domain.builders.StyleBuilder;
+import ar.com.fdvs.dj.domain.constants.Font;
+
+public class FontReportTest extends BaseDjReportTest {
+
+	public DynamicReport buildReport() throws Exception {
+
+
+		Font font = new Font(28,"Colonna MT","/fonts/COLONNA.TTF",Font.PDF_ENCODING_Identity_H_Unicode_with_horizontal_writing,true);
+		Style titleStyle = new StyleBuilder(false).setFont(font).build();
+		
+		/**
+		 * Creates the DynamicReportBuilder and sets the basic options for
+		 * the report
+		 */
+		FastReportBuilder drb = new FastReportBuilder();
+		drb.addColumn("State", "state", String.class.getName(),30)
+			.addColumn("Branch", "branch", String.class.getName(),30)
+			.addColumn("Product Line", "productLine", String.class.getName(),50)
+			.addColumn("Item", "item", String.class.getName(),50)
+			.addColumn("Item Code", "id", Long.class.getName(),30,true)
+			.addColumn("Quantity", "quantity", Long.class.getName(),60,true)
+			.addColumn("Amount", "amount", Float.class.getName(),70,true)
+			.addGroups(2)
+			.setTitle("November 2006 sales report")
+			.setTitleStyle(titleStyle)
+			.setSubtitle("This report was generated at " + new Date())
+			
+			.setUseFullPageWidth(true);
+
+		DynamicReport dr = drb.build();
+		
+		return dr;
+	}
+
+	public static void main(String[] args) throws Exception {
+		FontReportTest test = new FontReportTest();
+		test.testReport();
+		JasperViewer.viewReport(test.jp);	//finally display the report report
+//			JasperDesignViewer.viewReportDesign(jr);
+	}
+
+}
