4fde371d8af14e86fcac815960f1a8c5953e153a
djmamana <djmamana@2bfdcaec-5528-0410-9d21-f7f2b6497bdd>
2009-11-17 18:13:02 +0000

    git-svn-id: https://dynamicjasper.svn.sourceforge.net/svnroot/dynamicjasper/api/tags/release_3_0_13@851 2bfdcaec-5528-0410-9d21-f7f2b6497bdd

--- src/main/java/ar/com/fdvs/dj/domain/constants/BaseDomainConstant.java
+++
@@ -1,9 +0,0 @@
-package ar.com.fdvs.dj.domain.constants;
-
-import java.io.Serializable;
-
-public class BaseDomainConstant implements Serializable {
-
-	private static final long serialVersionUID = 1L;
-
-}
--- src/others/hsqldb.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-java -cp $M2_REPO/hsqldb/hsqldb/1.8.0.7/hsqldb-1.8.0.7.jar org.hsqldb.util.DatabaseManager
