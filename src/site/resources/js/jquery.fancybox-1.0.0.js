be08e33d9dc0de98e2d7d141ddcab5f7307ab0d9
Juan Manuel Alvarez <juan.alvarez@fdvsolutions.com>
2012-06-28 15:05:39 -0300

    - DJ 3.2.4 - BSD

--- src/site/resources/js/jquery.fancybox-1.0.0.js
+++ src/site/resources/js/jquery.fancybox-1.0.0.js
@@ -1,11 +1,29 @@
 /*
- * FancyBox - simple jQuery plugin for fancy image zooming
- * Examples and documentation at: http://fancy.klade.lv/
- * Version: 1.0.0 (29/04/2008)
- * Copyright (c) 2008 Janis Skarnelis
- * Licensed under the MIT License: http://www.opensource.org/licenses/mit-license.php
- * Requires: jQuery v1.2.1 or later
-*/
+ * Copyright (c) 2012, FDV Solutions S.A.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the FDV Solutions S.A. nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL FDV Solutions S.A. BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 (function($) {
 	var opts = {}, 
 		imgPreloader = new Image, imgTypes = ['png', 'jpg', 'jpeg', 'gif'], 
