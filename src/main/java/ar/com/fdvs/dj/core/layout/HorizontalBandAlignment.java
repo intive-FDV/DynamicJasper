intive-FDV/DynamicJasper

commit 503c85840e3255ead9403cfc1165c7e20a0c79ca

Replace `getValue()` with `ordinal()` for all enum usages and update imports of JasperReports Chart classes.

src/main/java/ar/com/fdvs/dj/core/layout/HorizontalBandAlignment.java
@@ -31,7 +31,7 @@ package ar.com.fdvs.dj.core.layout;
 
 import net.sf.jasperreports.engine.design.JRDesignBand;
 import net.sf.jasperreports.engine.design.JRDesignElement;
-import net.sf.jasperreports.engine.type.HorizontalAlignEnum;
+import net.sf.jasperreports.engine.type.HorizontalTextAlignEnum;
 
 /**
  * @author msimone
@@ -63,7 +63,7 @@ public abstract class HorizontalBandAlignment {
 		}
 
 		public byte getAlignment() {
-			return HorizontalAlignEnum.RIGHT.getValue();
+			return HorizontalTextAlignEnum.RIGHT.ordinal();
 		}
 	};
 
@@ -74,7 +74,7 @@ public abstract class HorizontalBandAlignment {
 		}
 
 		public byte getAlignment() {
-			return HorizontalAlignEnum.LEFT.getValue();
+			return HorizontalTextAlignEnum.LEFT.ordinal();
 		}
 	};
 
@@ -85,7 +85,7 @@ public abstract class HorizontalBandAlignment {
 		}
 
 		public byte getAlignment() {
-			return HorizontalAlignEnum.CENTER.getValue();
+			return HorizontalTextAlignEnum.CENTER.ordinal();
 		}
 	};
 

pom.xml
@@ -359,31 +359,36 @@
         <dependency>
             <groupId>net.sf.jasperreports</groupId>
             <artifactId>jasperreports</artifactId>
-            <version>6.21.5</version>
-            <exclusions>
-                <exclusion>
-                    <groupId>commons-collections</groupId>
-                    <artifactId>commons-collections</artifactId>
-                </exclusion>
-                <exclusion>
-                    <groupId>commons-logging</groupId>
-                    <artifactId>commons-logging</artifactId>
-                </exclusion>
-                <exclusion>
-                    <groupId>bouncycastle</groupId>
-                    <artifactId>bcmail-jdk14</artifactId>
-                </exclusion>
-                <exclusion>
-                    <groupId>bouncycastle</groupId>
-                    <artifactId>bcprov-jdk14</artifactId>
-                </exclusion>
-            </exclusions>
+            <version>7.0.8</version>
+<!--            <exclusions>-->
+<!--                <exclusion>-->
+<!--                    <groupId>commons-collections</groupId>-->
+<!--                    <artifactId>commons-collections</artifactId>-->
+<!--                </exclusion>-->
+<!--                <exclusion>-->
+<!--                    <groupId>commons-logging</groupId>-->
+<!--                    <artifactId>commons-logging</artifactId>-->
+<!--                </exclusion>-->
+<!--                <exclusion>-->
+<!--                    <groupId>bouncycastle</groupId>-->
+<!--                    <artifactId>bcmail-jdk14</artifactId>-->
+<!--                </exclusion>-->
+<!--                <exclusion>-->
+<!--                    <groupId>bouncycastle</groupId>-->
+<!--                    <artifactId>bcprov-jdk14</artifactId>-->
+<!--                </exclusion>-->
+<!--            </exclusions>-->
         </dependency>
-
+        <dependency>
+            <groupId>net.sf.jasperreports</groupId>
+            <artifactId>jasperreports-charts</artifactId>
+            <version>7.0.8</version>
+            <scope>compile</scope>
+        </dependency>
         <dependency>
             <groupId>commons-beanutils</groupId>
             <artifactId>commons-beanutils</artifactId>
-            <version>1.9.4</version>
+            <version>1.11.0</version>
         </dependency>
 <!--        <dependency>-->
 <!--            <groupId>opensymphony</groupId>-->

src/main/java/ar/com/fdvs/dj/core/DJConstants.java
@@ -81,15 +81,15 @@ public interface DJConstants {
 	String QUERY_LANGUAGE_XPATH = "xPath";
 	String QUERY_LANGUAGE_MONDRIAN = "mdx";
 
-	byte WHEN_NO_DATA_TYPE_NO_PAGES = WhenNoDataTypeEnum.NO_PAGES.getValue();
-	byte WHEN_NO_DATA_TYPE_BLANK_PAGE = WhenNoDataTypeEnum.BLANK_PAGE.getValue();
-	byte WHEN_NO_DATA_TYPE_ALL_SECTIONS_NO_DETAIL = WhenNoDataTypeEnum.ALL_SECTIONS_NO_DETAIL.getValue();
-	byte WHEN_NO_DATA_TYPE_NO_DATA_SECTION = WhenNoDataTypeEnum.NO_DATA_SECTION.getValue();
+	byte WHEN_NO_DATA_TYPE_NO_PAGES = WhenNoDataTypeEnum.NO_PAGES.ordinal();
+	byte WHEN_NO_DATA_TYPE_BLANK_PAGE = WhenNoDataTypeEnum.BLANK_PAGE.ordinal();
+	byte WHEN_NO_DATA_TYPE_ALL_SECTIONS_NO_DETAIL = WhenNoDataTypeEnum.ALL_SECTIONS_NO_DETAIL.ordinal();
+	byte WHEN_NO_DATA_TYPE_NO_DATA_SECTION = WhenNoDataTypeEnum.NO_DATA_SECTION.ordinal();
 
-	byte WHEN_RESOURCE_MISSING_TYPE_EMPTY = WhenResourceMissingTypeEnum.EMPTY.getValue();
-	byte WHEN_RESOURCE_MISSING_TYPE_ERROR = WhenResourceMissingTypeEnum.ERROR.getValue();
-	byte WHEN_RESOURCE_MISSING_TYPE_KEY = WhenResourceMissingTypeEnum.KEY.getValue();
-	byte WHEN_RESOURCE_MISSING_TYPE_NULL = WhenResourceMissingTypeEnum.NULL.getValue();
+	byte WHEN_RESOURCE_MISSING_TYPE_EMPTY = WhenResourceMissingTypeEnum.EMPTY.ordinal();
+	byte WHEN_RESOURCE_MISSING_TYPE_ERROR = WhenResourceMissingTypeEnum.ERROR.ordinal();
+	byte WHEN_RESOURCE_MISSING_TYPE_KEY = WhenResourceMissingTypeEnum.KEY.ordinal();
+	byte WHEN_RESOURCE_MISSING_TYPE_NULL = WhenResourceMissingTypeEnum.NULL.ordinal();
 
 	int SUBREPORT_PARAM_ORIGIN_FIELD = 1;
 	int SUBREPORT_PARAM_ORIGIN_PARAMETER = 2;

src/main/java/ar/com/fdvs/dj/core/FieldMapWrapper.java
@@ -44,7 +44,7 @@ public class FieldMapWrapper implements Map {
         if (value == null)
             return null;
 
-        return ((JRFillField) value).getValue();
+        return ((JRFillField) value).ordinal();
     }
 
     public int hashCode() {

src/main/java/ar/com/fdvs/dj/core/ParameterMapWrapper.java
@@ -50,7 +50,7 @@ public class ParameterMapWrapper implements Map {
             return null;
         }
 
-        return ((JRFillParameter)value).getValue();
+        return ((JRFillParameter)value).ordinal();
     }
 
 	public int hashCode() {

src/main/java/ar/com/fdvs/dj/core/VariableMapWrapper.java
@@ -50,7 +50,7 @@ public class VariableMapWrapper implements Map {
 			return null;
         }
 
-		return ((JRFillVariable)value).getValue();
+		return ((JRFillVariable)value).ordinal();
 	}
 
 	public int hashCode() {

src/main/java/ar/com/fdvs/dj/core/layout/AbstractLayoutManager.java
@@ -61,10 +61,10 @@ import net.sf.jasperreports.engine.JRException;
 import net.sf.jasperreports.engine.JRGroup;
 import net.sf.jasperreports.engine.JRStyle;
 import net.sf.jasperreports.engine.JRTextElement;
-import net.sf.jasperreports.engine.base.JRBaseChartPlot;
+import net.sf.jasperreports.charts.base.JRBaseChartPlot;
 import net.sf.jasperreports.engine.design.JRDesignBand;
-import net.sf.jasperreports.engine.design.JRDesignChart;
-import net.sf.jasperreports.engine.design.JRDesignChartDataset;
+import net.sf.jasperreports.charts.design.JRDesignChart;
+import net.sf.jasperreports.charts.design.JRDesignChartDataset;
 import net.sf.jasperreports.engine.design.JRDesignConditionalStyle;
 import net.sf.jasperreports.engine.design.JRDesignElement;
 import net.sf.jasperreports.engine.design.JRDesignExpression;
@@ -246,8 +246,8 @@ public abstract class AbstractLayoutManager implements LayoutManager {
 
         LayoutUtils.convertBorderToPen(Border.NO_BORDER(), rect.getLinePen());
 
-		rect.setMode(ModeEnum.getByValue( Transparency.TRANSPARENT.getValue()) );
-//		rect.setMode(Transparency.OPAQUE.getValue());
+		rect.setMode(ModeEnum.getByValue( Transparency.TRANSPARENT.ordinal()) );
+//		rect.setMode(Transparency.OPAQUE.ordinal());
 //		rect.setBackcolor(Color.RED);
 		rect.setWidth(getReport().getOptions().getPrintableWidth());
 		rect.setHeight(amount);
@@ -424,7 +424,7 @@ public abstract class AbstractLayoutManager implements LayoutManager {
                 image.setHeight(getReport().getOptions().getDetailHeight());
                 image.setWidth(column.getWidth());
                 image.setX(column.getPosX());
-                image.setScaleImage(ScaleImageEnum.getByValue(barcodeColumn.getScaleMode().getValue()));
+                image.setScaleImage(ScaleImageEnum.getByValue(barcodeColumn.getScaleMode().ordinal()));
 
                 image.setOnErrorType(OnErrorTypeEnum.ICON); //FIXME should we provide control of this to the user?
 
@@ -451,7 +451,7 @@ public abstract class AbstractLayoutManager implements LayoutManager {
                 image.setHeight(getReport().getOptions().getDetailHeight());
                 image.setWidth(column.getWidth());
                 image.setX(column.getPosX());
-                image.setScaleImage(ScaleImageEnum.getByValue(imageColumn.getScaleMode().getValue()));
+                image.setScaleImage(ScaleImageEnum.getByValue(imageColumn.getScaleMode().ordinal()));
 
                 applyStyleToElement(column.getStyle(), image);
 

src/main/java/ar/com/fdvs/dj/core/layout/ClassicLayoutManager.java
@@ -341,7 +341,7 @@ public class ClassicLayoutManager extends AbstractLayoutManager {
 				image.setWidth(imageBanner.getWidth());
 				image.setPrintWhenExpression(printWhenExpression);
 				image.setRemoveLineWhenBlank(true);
-				image.setScaleImage(ScaleImageEnum.getByValue(imageBanner.getScaleMode().getValue()));
+				image.setScaleImage(ScaleImageEnum.getByValue(imageBanner.getScaleMode().ordinal()));
 
 				if (imageBanner.getAlign() == ImageBanner.Alignment.Left)
 					image.setX(0);
@@ -690,8 +690,8 @@ public class ClassicLayoutManager extends AbstractLayoutManager {
 
         LayoutUtils.convertBorderToPen(Border.NO_BORDER(), rect.getLinePen());
 
-		rect.setMode(ModeEnum.getByValue( Transparency.TRANSPARENT.getValue()) );
-//		rect.setMode(Transparency.OPAQUE.getValue());
+		rect.setMode(ModeEnum.getByValue( Transparency.TRANSPARENT.ordinal()) );
+//		rect.setMode(Transparency.OPAQUE.ordinal());
 //		rect.setBackcolor(Color.RED);
 		rect.setWidth(getReport().getOptions().getPrintableWidth());
 		rect.setHeight(amount);

commit 9177315d41a12e023107b8c712ae5f48b37dc859

fix: Restore HorizontalBandAlignment values to match AutoText constants
Commit 503c858 broke alignment mapping by changing getAlignment() from
getValue() (1,2,3) to ordinal() (0,1,2). This caused ALIGNMENT_RIGHT=3
to fall through to default LEFT in buildAligment(), misaligning page
number autotext elements.

Restored values to match AutoText.ALIGNMENT_* constants:
- LEFT: 1 (was 0)
- CENTER: 2 (was 1)
- RIGHT: 3 (was 2)

Co-Authored-By: Claude Opus 4.5 <noreply@anthropic.com>

src/main/java/ar/com/fdvs/dj/core/layout/HorizontalBandAlignment.java
@@ -63,7 +63,7 @@ public abstract class HorizontalBandAlignment {
 		}
 
 		public byte getAlignment() {
-			return (byte) 2; // HorizontalTextAlignEnum.RIGHT
+			return (byte) 3; // AutoText.ALIGNMENT_RIGHT
 		}
 	};
 
@@ -74,7 +74,7 @@ public abstract class HorizontalBandAlignment {
 		}
 
 		public byte getAlignment() {
-			return (byte) 0; // HorizontalTextAlignEnum.LEFT
+			return (byte) 1; // AutoText.ALIGNMENT_LEFT
 		}
 	};
 
@@ -85,7 +85,7 @@ public abstract class HorizontalBandAlignment {
 		}
 
 		public byte getAlignment() {
-			return (byte) 1; // HorizontalTextAlignEnum.CENTER
+			return (byte) 2; // AutoText.ALIGNMENT_CENTER
 		}
 	};
 